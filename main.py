5110cea04a790cdf6eec0c56e69a8f1255f7efe8
1Kuznetsov <i.kuznetsov5@g.nsu.ru>
2023-04-12 20:33:39 +0700

    The beginning

--- main.py
+++ main.py
@@ -2,15 +2,29 @@ import ru_local as ru
 
 # Kuznetsov Igor , Yadreeva Maria
 
-total_w = 0
+t_salary = 0
+t_tax = 0
+t_income = 0
+tax_lot = 0
 month = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
          "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
 
 for num in range(12):
     print(ru.SALARY, f"{month[num]}: ", end='')
-    total_w += float(input())
+    t_salary += float(input())
+
+days = int(input(ru.DAYS))
+prize = float(input(ru.PRIZE))
+prize_ad = float(input(ru.PRIZE_AD))
 
 investing = float(input(ru.INVESTING))
 deposits = float(input(ru.DEPOSIT))
 max_key_rate = float(input(ru.MAX_KEY_RATE))
-days = int(input(ru.DAYS))
+if deposits <= 1_000_000 * max_key_rate:
+    print("Налог уплачивать не нужно")
+else:
+    tax_d = (deposits - 1_000_000 * max_key_rate) * 0.13
+    print(f"Налог составит {tax_d} рублей")
+    t_tax += tax_d
+estate = float(input(ru.ESTATE))
+t_income += t_salary + investing + deposits
